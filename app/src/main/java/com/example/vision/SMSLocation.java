f59dac914e29edbea1f8f1ddf7bbf0556aa0666b
Fatima Pereira <8690.fatima.it@gmail.com>
2022-03-06 15:41:49 +0530

    Changes done on 06-03-2022 @3:23PM modified location feature

--- app/src/main/java/com/example/vision/SMSLocation.java
+++ app/src/main/java/com/example/vision/SMSLocation.java
@@ -3,27 +3,21 @@ package com.example.vision;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.PendingIntent;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.telephony.SmsManager;
-import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -33,6 +27,7 @@ public class SMSLocation extends AppCompatActivity {
     private static String myLocation;
     private static ProgressDialog progressDialog;
     private static boolean isSmsSent = false;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,108 +45,122 @@ public class SMSLocation extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        if(!isSmsSent)
+        if(!isSmsSent) {
             checkAppPermissions();
+        }
     }
 
-    void checkAppPermissions(){
+    void checkAppPermissions() {
 
         ArrayList<String> permissionsList = new ArrayList<>();
         String[] permissionsArray;
 
         //Location permission
-        if((ActivityCompat.checkSelfPermission(getApplicationContext(),
+        if ((ActivityCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
                 && (ActivityCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)){
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             permissionsList.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
         //SMS permission
-        if(ActivityCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             permissionsList.add(Manifest.permission.SEND_SMS);
         }
 
         //Converting the permissions list to array
         permissionsArray = new String[permissionsList.size()];
-        for(int i=0; i<permissionsList.size(); i++){
+        for (int i = 0; i < permissionsList.size(); i++) {
             permissionsArray[i] = permissionsList.get(i);
         }
 
         //Requesting only those app permissions that are not yet granted
-        if(permissionsArray.length > 0) {
+        if (permissionsArray.length > 0) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(permissionsArray, 6000);
             }
             return;
         }
 
-        sendLocation();
+        checkLocationAccess();
     }
 
-    public void sendLocation(){
+    public void checkLocationAccess(){
 
-        //Checking location permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            //Requesting Location permission
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    100);
-            return;
-        }
+        //Checking if GPS option is enabled on the mobile
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         //if Location access is not enabled
-        if(!(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))){
+        if (!locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) &&
+                !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 
             startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+
             return;
         }
-        else{
 
-            new TTS(this,"Please wait while we fetch your current Location. This may take a few seconds");
-            progressDialog = ProgressDialog.show(this, "Fetching Location","Please wait...",true);
+        boolean isNetworkProvider, isGPSProvider;
 
-            Toast.makeText(getApplicationContext(), "GPS ENABLED", Toast.LENGTH_SHORT).show();
+        isNetworkProvider = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        isGPSProvider = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
+        if(isNetworkProvider) {
+            //getting location using Network is faster compared to GPS however the result may not be that accurate
+            getLocation("network");
+        }
+        else if(isGPSProvider) {
+            //getting location using GPS is quite time consuming even though it gives quite accurate results
+            getLocation("gps");
+        }
 
-                    progressDialog.dismiss();
-                    if (location != null) {
+    }
 
-                        // Logic to handle location
+    @SuppressLint("MissingPermission")
+    void getLocation(String Provider){
+
+        new TTS(this, "Please wait while we fetch and send your current location to your emergency contact numbers. This may take a few seconds");
+        progressDialog = ProgressDialog.show(this, "Fetching location and sending SMS", "Please wait...", true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //here provider can be Network or GPS based on whichever one is available
+        locationManager.requestSingleUpdate(Provider, new LocationListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                if (location != null) {
+
+                    // Logic to handle location
+                    if(location != null) {
                         myLocation = "http://maps.google.com/maps?q=loc:" + location.getLatitude() + "," + location.getLongitude();
                         sendSMS();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Unable to find location :(", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "LOCATION: NULL", Toast.LENGTH_SHORT).show();
                     }
                 }
+            }
 
-                @Override
-                public void onProviderEnabled(@NonNull String provider) { }
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+            }
 
-                @Override
-                public void onProviderDisabled(@NonNull String provider) { }
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+            }
 
-                @Override
-                public void onStatusChanged(String provider, int status, Bundle extras) { }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
 
-            }, Looper.myLooper());
-        }
+        }, Looper.myLooper());
     }
 
     void sendSMS(){
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
         String scAddress = null;
         String num1, num2, num3;
 
@@ -178,57 +187,17 @@ public class SMSLocation extends AppCompatActivity {
             num3 = sharedPreferences.getString("num3", "NONE");
         }
 
-        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
-                new Intent(SENT), 0);
-
-        //---when the SMS has been sent--- is working alright
-        registerReceiver(new BroadcastReceiver()
-        {
-            public void onReceive(Context arg0, Intent arg1)
-            {
-                progressDialog.dismiss();
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS sent",
-                                Toast.LENGTH_SHORT).show();
-                        isSmsSent = true;
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(getBaseContext(), "Generic failure",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(getBaseContext(), "No service",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(getBaseContext(), "Null PDU",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        Toast.makeText(getBaseContext(), "Radio off",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-                unregisterReceiver(this);
-                startActivity(new Intent().setClass(getApplicationContext(), CallGuardian.class));
-                //finish();
-
-            }
-        }, new IntentFilter(SENT));
-
-        progressDialog = ProgressDialog.show(this, "Sending SMS. ","Please wait...",true);
-        new TTS(this,"Please wait while we send an SMS of your current location to your emergency contact numbers");
-
         Toast.makeText(getApplicationContext(), "Sending SMS", Toast.LENGTH_SHORT).show();
 
         //Sending SMS
         SmsManager manager = SmsManager.getDefault();
-        manager.sendTextMessage(num1, scAddress, "My phone's battery is low. This is my current location :\n" + myLocation, sentPI, null);
+        manager.sendTextMessage(num1, scAddress, "My phone's battery is low. This is my current location :\n" + myLocation, null, null);
         manager.sendTextMessage(num2, scAddress, "My phone's battery is low. This is my current location :\n" + myLocation, null, null);
         manager.sendTextMessage(num3, scAddress, "My phone's battery is low. This is my current location :\n" + myLocation, null, null);
 
+        progressDialog.dismiss();
+        isSmsSent = true;
+        startActivity(new Intent().setClass(this, CallGuardian.class));
     }
 
     @Override
@@ -236,18 +205,6 @@ public class SMSLocation extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if(requestCode == 6000){
-            checkAppPermissions();
-        }
-
-        //Location
-        if (requestCode == 100 & grantResults.length > 0 && (grantResults[0] + grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(getApplicationContext(), "Location Permission Granted", Toast.LENGTH_SHORT).show();
-            checkAppPermissions();
-        }
-
-        //SMS
-        if (requestCode == 101 && grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
-            Toast.makeText(getApplicationContext(), "SMS Permission Granted", Toast.LENGTH_SHORT).show();
             checkAppPermissions();
         }
     }
